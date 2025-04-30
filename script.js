a11ed2fb13c608e44d672caafe3d16c1b7aa3368
ReadyPancake420 <112571483+ReadyPancake420@users.noreply.github.com>
2025-04-29 23:02:04 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -127,7 +127,7 @@
 
     var textAnimate = eval(Jscex.compile("async", function () {
         var together = new Date();
-        together.setFullYear(2025, 3, 2);
+        together.setFullYear(2025, 2, 2);
         together.setHours(0);
         together.setMinutes(0);
         together.setSeconds(0);
